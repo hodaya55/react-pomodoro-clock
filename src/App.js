436eb3267a0e0220de8eea40602ed7f1b01654cc
hodaya55 <hodayah159@gmail.com>
2018-07-23 00:07:20 +0300

    adding stop and rest button (working)

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ const Border = (props) => <div className={'FancyBorder'}>{props.children} </div>
 class Clock extends Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date(), counterTimer: 0, showTimer: '00:00', isEnd: false, isStart: false, isSelect: false };
-    // this.isStart = false;
+    this.state = { date: new Date(), counterTimer: 0, showTimer: '00:00', isStart: false, isSelect: false, isStop: false, isReset: false };
   }
   // this function will be invoked a setInterval function immediately after the view is ready and rendered.
   componentDidMount() {
@@ -20,23 +19,19 @@ class Clock extends Component {
   }
   down() {
     let count = this.state.counterTimer;
-    let isEnd = this.state.isEnd;
-    if (!isEnd) {
+    if (count > 0) {
       count--;
       let m = Math.floor(count / 60);
       let s = count % 60;
-      s < 10 ? s = '0' + s : null;
-      let showTimer = `${m}:${s}`;
-      count === 0 ? (isEnd = true) : (isEnd = false);
+      let showTimer = `${m < 10 ? ('0' + m) : m}:${s < 10 ? ('0' + s) : s}`;
       this.setState(() => {
-        return { counterTimer: count, showTimer: showTimer, isEnd: isEnd };
+        return { counterTimer: count, showTimer: showTimer };
       });
     }
-    else {
+    else { // timer is over
       clearInterval(this.countDown);
-
       this.setState(() => {
-        return { isSelect: false, isStart: false, isEnd: false };
+        return { isSelect: false, isStart: false };
       });
     }
 
@@ -47,7 +42,8 @@ class Clock extends Component {
     clearInterval(this.countDown);
   }
   selectTimer = (e) => {
-    let showTimer = `${e.target.value}:00`;
+    let { value } = e.target;
+    let showTimer = `${value < 10 ? ('0' + value) : value}:00`;
     this.setState({ counterTimer: e.target.value * 60, showTimer: showTimer, isSelect: true });
   }
   startTimer = () => {
@@ -56,21 +52,34 @@ class Clock extends Component {
     this.setState({ isStart: true });
     this.countDown = setInterval(() => this.down(), 1000);
   }
+  stopTimer = () => {
+    clearInterval(this.countDown);
+    this.setState({ isStop: true, isStart: false });
+  }
+  resetTimer = () => {
+    clearInterval(this.countDown);
+    this.setState({ showTimer: '00:00', isSelect: false, isStart: false, counterTimer: 0 });
+  }
   render() {
     return (
       <React.Fragment>
         <Border>
-          {/* <h2>It is {this.state.date.toLocaleTimeString()}</h2> */}
+          {/* <h2>It is {this.state.date.toLocaleTimeString()}    now</h2> */}
           <h2> Timer Break</h2>
         </Border>
         <p>Choose time:</p>
         <button onClick={this.selectTimer} value="25">25</button>
+        <button onClick={this.selectTimer} value="20">20</button>
+        <button onClick={this.selectTimer} value="15">15</button>
+        <button onClick={this.selectTimer} value="10">10</button>
+        <button onClick={this.selectTimer} value="5">5</button>
         <button onClick={this.selectTimer} value="1">1</button>
         <hr />
         <h2 className="timer">
-          {(!this.state.isEnd && this.state.isSelect) ? this.state.showTimer : '00:00'}
+          {
+            (this.state.isStart && this.state.isSelect) ? this.state.showTimer : this.state.isSelect ? this.state.showTimer : '00:00'
+          }
         </h2>
-
         <div className="actions">
           <button disabled={this.state.isStart} onClick={this.startTimer} >Start >></button>
           <button onClick={this.stopTimer} >Stop ||</button>
